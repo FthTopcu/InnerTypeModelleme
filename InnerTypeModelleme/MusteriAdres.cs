0c55fd777b8756571f0b33820de1e576cd722cd0
FthTopcu <fthtpu@gmail.com>
2021-12-01 21:21:27 +0300

    da

--- InnerTypeModelleme/MusteriAdres.cs
+++ InnerTypeModelleme/MusteriAdres.cs
@@ -8,6 +8,18 @@ namespace InnerTypeModelleme
 {
     public class MusteriAdres
     {
+        public string AdresTip { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string Ardres { get; set; }
 
+        public MusteriAdres()
+        {
+            Console.WriteLine("Musteri Adres Çalıştı.");
+        }
+        public void MusteriAdresTestMetot()
+        {
+            Console.WriteLine("MusteriAdresTestMetot");
+        }
     }
 }
--- InnerTypeModelleme/Urun.cs
+++ InnerTypeModelleme/Urun.cs
@@ -8,6 +8,18 @@ namespace InnerTypeModelleme
 {
     public class Urun
     {
+        public int UrunId { get; set; }
+        public string Tanim { get; set; }
+        public decimal Fiyat { get; set; }
+
+        public Urun()
+        {
+            Console.WriteLine("Ürün Yapıcı Metot Çalıştı.");
+        }
+        public void UrunTestMetot()
+        {
+            Console.WriteLine("UrunTestMetot");
+        }
 
     }
 }
